12982cfb0efb019d60f09e6b73ff794ef393af35
derrickhamilton <derrickhamilton212@gmail.com>
2026-07-28 20:05:33 -0400

    increased complexity of the questions.json file and updated question submission logic

--- questionSubmissionForm.py
+++ questionSubmissionForm.py
@@ -11,7 +11,7 @@ from datetime import date
 
 questionFormTitleString = "Submit a question!"
  
-def handleQuestionSubmission(authorString, questionContentString):
+def handleQuestionSubmission(authorString, questionContentString, serverNameString):
     questionsData = {}
 
     # Define a dictionary structure using the questions.json file created by AMA-bot
@@ -21,8 +21,16 @@ def handleQuestionSubmission(authorString, questionContentString):
     # Get today's date in yyyy-mm-dd format
     today = date.today().isoformat()
 
-    newQuestionData = {"author": authorString, "dateSubmitted": today, "questionContent": questionContentString}
-    questionsData["questions"].append(newQuestionData)
+    i = 0;
+    for server in questionsData.get("servers"):
+        serverNameValue = server.get("server-name", "Unknown")
+        print("Compare to value: " + serverNameString + " retrieved value: " + serverNameValue)
+        if serverNameString == serverNameValue:
+            print("Found server! Name: " + serverNameString)
+            newQuestionData = {"author": authorString, "dateSubmitted": today, "questionContent": questionContentString, "asked": False}
+            questionsData["servers"][i]["questions"].append(newQuestionData)
+            break;
+        i += 1
 
     with open("questions.json", "w") as newQuestionsFile:
         json.dump(questionsData, newQuestionsFile, indent=4)
@@ -37,9 +45,10 @@ class QuestionSubmissionForm(discord.ui.Modal, title=questionFormTitleString):
 
     async def on_submit(self, interaction: discord.Interaction):
         # Need to handle the text submitted through the form
-        print(f"Submission from user {interaction.user.display_name}: {self.msg_content.value}")
+        print(f"Submission from user {interaction.user.display_name}: {self.msg_content.value}\nServer: {interaction.guild}")
 
-        handleQuestionSubmission(interaction.user.display_name, self.msg_content.value)
+        assert interaction.guild is not None
+        handleQuestionSubmission(interaction.user.display_name, self.msg_content.value, interaction.guild.name)
 
         await interaction.response.send_message(f"{interaction.user.mention} - Thank you for submitting a question!")
 
@@ -49,5 +58,5 @@ class LaunchQuestionSubmissionFormView(discord.ui.View):
         super().__init__(timeout=timeout)
 
     @discord.ui.button(label="Submit", style=discord.ButtonStyle.primary, custom_id="open_form_button")
-    async def open_form(self, interaction: discord.Interation, button: discord.ui.Button):
+    async def open_form(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_modal(QuestionSubmissionForm())
